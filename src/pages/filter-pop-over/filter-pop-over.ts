f3e3fbe47123f6ed83af101b3d88e40498205c72
Louw Visagie <louw.visagie@geminisolution.co.za>
2018-09-19 10:40:25 +0200

    Progress commit

--- src/pages/filter-pop-over/filter-pop-over.ts
+++ src/pages/filter-pop-over/filter-pop-over.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { HomePage } from '../../pages/home/home'
+import { HomePage } from '../../pages/home/home';
 import * as _ from 'lodash';
 /**
  * Generated class for the FilterPopOverPage page.
@@ -15,22 +15,16 @@ import * as _ from 'lodash';
   templateUrl: 'filter-pop-over.html'
 })
 export class FilterPopOverPage {
-  private homePage:HomePage;
+  private homePage: HomePage;
+  public filtered: boolean = false;
   agents: Array<any>;
-  managementType:string;
+  filteredAgents: Array<any>;
+  managementType: string;
   selectedAgents: any[] = [];
   public constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.homePage = navParams.get('homePage');
   }
-  ngOnInit() {
-    this.agents = [
-      { id: 1, name: 'Caitlin Parry', selected: false },
-      { id: 2, name: 'Hayden Barlow', selected: false },
-      { id: 3, name: 'Merle Zimmerman', selected: false },
-      { id: 4, name: 'Ning Coles', selected: false },
-      { id: 5, name: 'Peter Burke', selected: false }
-    ];
-  }
+  ngOnInit() {}
   setItems() {
     this.agents = [
       { id: 1, name: 'Caitlin Parry', selected: false },
@@ -50,22 +44,22 @@ export class FilterPopOverPage {
     let val = ev.target.value;
 
     if (val && val.trim() !== '') {
-      this.agents = this.agents.filter(function(item) {
+      this.filteredAgents = this.agents.filter(function(item) {
         return item.name.toLowerCase().includes(val.toLowerCase());
       });
     }
   }
 
-  public setAgent(index) :void {
-   // debugger;
-   
-   this.agents[index].selected = !this.agents[index].selected;
-   this.selectedAgents = _.filter(this.agents, {selected:true});
-   console.log(this.selectedAgents); 
+  public setAgent(index): void {
+    this.filteredAgents[index].selected = !this.filteredAgents[index].selected;
+    this.selectedAgents = _.filter(this.filteredAgents, { selected: true });
+    this.filtered = true;
+    console.log(this.selectedAgents);
   }
 
   setManagementType(event) {
     console.log(event);
-    this.managementType = event.target.value;
+    this.managementType = event;
+    this.filtered = true;
   }
 }
